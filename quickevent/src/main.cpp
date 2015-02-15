f9eb08ceb2b3aa9c4de29c34e87e3ed8df316744
Fanda Vacek <fanda.vacek@gmail.com>
2015-02-15 22:56:41 +0100

    quickevent: --help option added

--- quickevent/src/main.cpp
+++ quickevent/src/main.cpp
@@ -6,7 +6,8 @@
 
 #include <QtQml>
 #include <QLocale>
-//#include <qqml.h>
+
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
@@ -23,6 +24,26 @@ int main(int argc, char *argv[])
 
 	qDebug() << "creating application instance";
 	Application app(argc, argv);
+
+	bool o_help = false;
+
+	QStringList args = app.arguments();
+	for(int i=0; i<args.count(); i++) {
+		QString arg = args.value(i);
+		if(arg == QLatin1String("--help") || arg == QLatin1String("-h")) {
+			o_help = true;
+		}
+	}
+
+	if(o_help) {
+		std::cout << args.value(0).toStdString() << " [OPTIONS]" << std::endl << std::endl;
+		std::cout << "OPTIONS:" << std::endl;
+		std::cout << "--profile <path_to_profile_file>\tloads specific profile (see: https://github.com/fvacek/quickbox/wiki/Application-profiles)" << std::endl;
+		std::cout << "-d[domain[:LEVEL]]\tset debug domain and level" << std::endl;
+		std::cout << "\t\t\tdomain: any substring of source module, for example 'mymod' prints debug info from every source file with name containing 'mymod', mymodule.cpp, tomymod.cpp, ..." << std::endl;
+		std::cout << "\t\t\tLEVEL: any of DEB, INFO, WARN, ERR, default level is INFO" << std::endl;
+		return 0;
+	}
 
 	MainWindow main_window;
 	//QObject::connect(signal_log_device.data(), &qf::core::SignalLogDevice::logEntry, &main_window, &MainWindow::logEntry, Qt::QueuedConnection);
