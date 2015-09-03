6465e22275b06f1a7ea032408317ffd42a45ceb9
Fanda Vacek <vacek@elektroline.cz>
2015-09-03 16:23:55 +0200

    qfcore: log.h splitted to log.h and logcore.h
    * this enables use qf::core::Log::Level in header files

--- libqf/libqfcore/src/core/log.h
+++ libqf/libqfcore/src/core/log.h
@@ -1,7 +1,7 @@
 #ifndef QF_CORE_LOG_H
 #define QF_CORE_LOG_H
 
-#include "coreglobal.h"
+#include "logcore.h"
 
 #include <QDebug>
 
@@ -48,34 +48,5 @@
 #define qfLogFuncFrame() QDebug __func_frame_exit_logger__ = QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO).debug() << "     EXIT FN" << Q_FUNC_INFO; \
 	qfDebug() << ">>>> ENTER FN" << Q_FUNC_INFO
 #endif
-
-class QLoggingCategory;
-
-namespace qf {
-namespace core {
-
-class QFCORE_DECL_EXPORT Log
-{
-public:
-	enum class Level : int { Invalid = -1, Fatal, Error, Warning, Info, Debug };
-public:
-	static const char* categoryDebugName;
-	static const char* categoryInfoName;
-	static const char* categoryWarningName;
-	static const char* categoryErrorName;
-	static const char* categoryFatalName;
-
-	static const QLoggingCategory& categoryForLevel(Level level);
-	static const char *levelName(Level level);
-
-	static void checkLogLevelMetaTypeRegistered();
-public:
-	static QString stackTrace();
-};
-
-}
-}
-
-Q_DECLARE_METATYPE(qf::core::Log::Level)
 
 #endif
---
+++ libqf/libqfcore/src/core/logcore.h
@@ -0,0 +1,37 @@
+#ifndef QF_CORE_LOGCORE_H
+#define QF_CORE_LOGCORE_H
+
+#include "coreglobal.h"
+
+#include <QMetaType>
+
+class QLoggingCategory;
+
+namespace qf {
+namespace core {
+
+class QFCORE_DECL_EXPORT Log
+{
+public:
+	enum class Level : int { Invalid = -1, Fatal, Error, Warning, Info, Debug };
+public:
+	static const char* categoryDebugName;
+	static const char* categoryInfoName;
+	static const char* categoryWarningName;
+	static const char* categoryErrorName;
+	static const char* categoryFatalName;
+
+	static const QLoggingCategory& categoryForLevel(Level level);
+	static const char *levelName(Level level);
+
+	static void checkLogLevelMetaTypeRegistered();
+public:
+	static QString stackTrace();
+};
+
+}
+}
+
+Q_DECLARE_METATYPE(qf::core::Log::Level)
+
+#endif
