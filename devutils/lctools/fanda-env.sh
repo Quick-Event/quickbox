a8393a2abe1a8da96fe867f3f76de374f9cf67a5
Fanda Vacek <fanda.vacek@gmail.com>
2015-12-01 15:42:38 +0100

    quickbox: localization folder renamed to lctools

--- devutils/lctools/fanda-env.sh
+++ devutils/lctools/fanda-env.sh
@@ -9,4 +9,4 @@ QT_HOME=$FANDA_HOME/programs/qt5/5.5/gcc_64/bin
 TS_HOME=$FANDA_HOME/t/${PROJ_NAME}-ts
 PROJ_BUILD=$PROJ_HOME/../_build/${PROJ_NAME}-release
 QM_DEST_DIR=$PROJ_BUILD/bin/translations
-LCTOOL_HOME=$PROJ_HOME/devutils/localization
+LCTOOL_HOME=$PROJ_HOME/devutils/lctools
