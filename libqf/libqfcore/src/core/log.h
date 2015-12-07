ad3a3a61444a5c301e9a6ee13542296e1d124afc
Fanda Vacek <fanda@localhost.localdomain>
2015-12-07 09:05:38 +0100

    libqfcore: log.h simplyfied

--- libqf/libqfcore/src/core/log.h
+++ libqf/libqfcore/src/core/log.h
@@ -8,7 +8,7 @@
 #ifndef QT_DEBUG
 #define NO_QF_DEBUG
 #endif
-
+/*
 #ifdef NO_QF_DEBUG
 #define qfDebug_q while(0) qDebug
 #else
@@ -21,30 +21,39 @@
 
 //#define qfLog_q(level) QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO).warning(qf::core::Log::categoryForLevel(level))
 
-#if (QT_VERSION < QT_VERSION_CHECK(5, 4, 0))
+#if (QT_VERSION < QT_VERSION_CHECK(5, 3, 0)) || (QT_VERSION >= QT_VERSION_CHECK(5, 5, 0))
 #define qfDebug qfDebug_q()
 #define qfInfo qfInfo_q()
 #define qfWarning qfWarning_q()
 #define qfError qfError_q()
 #define qfFatal qfFatal_q
-#define qfLog(level) qfLog_q(level)
+//#define qfLog(level) qfLog_q(level)
 #else
 #define qfDebug qfDebug_q().noquote
 #define qfInfo qfInfo_q().noquote
 #define qfWarning qfWarning_q().noquote
 #define qfError qfError_q().noquote
 #define qfFatal qfFatal_q
-#define qfLog(level) qfLog_q(level).noquote()
+//#define qfLog(level) qfLog_q(level).noquote()
 #endif
+*/
+
+#define qfFatal if(qCritical() << qf::core::Log::stackTrace(), true) qFatal
 
 #ifdef NO_QF_DEBUG
 #define qfCDebug(category) while(0) QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO, category).debug
 #else
 #define qfCDebug(category) QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO, category).debug
 #endif
+
 #define qfCInfo(category) QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO, category).info
 #define qfCWarning(category) QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO, category).warning
-#define qfCError(category) QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO, category).error
+#define qfCError(category) QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO, category).critical
+
+#define qfDebug qfCDebug("")
+#define qfInfo qfCInfo("")
+#define qfWarning qfCWarning("")
+#define qfError qfCError("")
 
 #ifdef NO_QF_DEBUG
 #define qfLogFuncFrame() while(0) qDebug()
