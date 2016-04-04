ddd914f4128aa99c81c7ced88804fb200d548d91
Martin Petricek <mp@petricek.net>
2016-04-04 20:55:52 +0200

    Add 3 new modes for start generation based on actual competition rules.

--- quickevent/app/plugins/qml/Runs/src/runswidget.h
+++ quickevent/app/plugins/qml/Runs/src/runswidget.h
@@ -46,6 +46,7 @@ public:
 	Q_SIGNAL void selectedStageIdChanged(int stage_id);
 
 	//void editStartList(int class_id, int competitor_id);
+
 private slots:
 	void on_btDraw_clicked();
 	void on_btDrawRemove_clicked();
@@ -61,13 +62,26 @@ private:
 	QList< QList<int> > runnersByClubSortedByCount(int stage_id, int class_id, QMap<int, QString> &runner_id_to_club);
 	QList<int> runsForClass(int stage_id, int class_id);
 	QMap<int, int> competitorsForClass(int stage_id, int class_id);
+    QList<int> runsForClassCondition(int stage_id, int class_id, QString condition);
+    QMap<int, int> competitorsForClassCondition(int stage_id, int class_id, QString condition);
+    QMap<int, int> runCompetitorQuery(QString query);
 
 	bool isLockedForDrawing(int class_id, int stage_id);
 	void saveLockedForDrawing(int class_id, int stage_id, bool is_locked);
 
 	void import_start_times_ob2000();
 private:
-	enum class DrawMethod : int {Invalid = 0, RandomNumber, EquidistantClubs, RandomizedEquidistantClubs, StageReverseOrder, Handicap};
+    enum class DrawMethod : int {
+        Invalid = 0,
+        RandomNumber, // Completely randomly
+        EquidistantClubs,
+        RandomizedEquidistantClubs,
+        StageReverseOrder,
+        Handicap,
+        GroupedC,  // All C first, followed by B + A (H/D 12,14)
+        GroupedCB, // All C first, then B, then A + E + R
+        GroupedRanking // First group ranking >300, then 101-300, then 1-100
+    };
 
 	Ui::RunsWidget *ui;
 	qf::qmlwidgets::ForeignKeyComboBox *m_cbxClasses = nullptr;
