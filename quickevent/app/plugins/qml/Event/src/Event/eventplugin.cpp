Quick-Event/quickbox

commit 78865b742dd7170418960204ddd14247138f55b1

QE: [#150 done] Výchozí datum etapy [#132 done] Check/Repair old version data on open

quickevent/app/plugins/qml/Event/src/Event/eventplugin.cpp
@@ -126,31 +126,25 @@ int EventPlugin::currentStageId()
 
 int EventPlugin::stageStart(int stage_id)
 {
-	Event::StageData stage_data = stageData(stage_id);
-	QTime start_time = stage_data.startTime();
+	QTime start_time = stageStartTime(stage_id);
 	int ret = start_time.msecsSinceStartOfDay();
 	return ret;
 }
 
 QDate EventPlugin::stageStartDate(int stage_id)
 {
-	Event::StageData stage_data = stageData(stage_id);
-	return stage_data.startDate();
+	return stageStartDateTime(stage_id).date();
 }
 
 QTime EventPlugin::stageStartTime(int stage_id)
 {
-	Event::StageData stage_data = stageData(stage_id);
-	return stage_data.startTime();
+	return stageStartDateTime(stage_id).time();
 }
 
 QDateTime EventPlugin::stageStartDateTime(int stage_id)
 {
 	Event::StageData stage_data = stageData(stage_id);
-	QTime t = stage_data.startTime();
-	QDate d = stage_data.startDate();
-	QDateTime dt(d, t);
-	//qfInfo() << stage_id << d << t << dt;
+	QDateTime dt = stage_data.startDateTime();
 	return dt;
 }
 /*
@@ -355,6 +349,11 @@ DbSchema EventPlugin::dbSchema()
 	return DbSchema(this);
 }
 
+int EventPlugin::minDbVersion()
+{
+	return 10005;
+}
+
 void EventPlugin::onDbEvent(const QString &name, QSqlDriver::NotificationSource source, const QVariant &payload)
 {
 	qfLogFuncFrame() << "name:" << name << "source:" << source << "payload:" << payload;
@@ -381,6 +380,23 @@ void EventPlugin::onDbEvent(const QString &name, QSqlDriver::NotificationSource 
 		else {
 			//qfDebug() << "self db notify";
 		}
+	}
+}
+
+void EventPlugin::repairStageStarts(const qf::core::sql::Connection &from_conn, const qf::core::sql::Connection &to_conn)
+{
+	qfs::Query to_q(to_conn);
+	qfs::Query from_q(from_conn);
+	from_q.exec("SELECT * FROM stages ORDER BY id");
+	while(from_q.next()) {
+		int ix = from_q.fieldIndex(QStringLiteral("startDate"));
+		if(ix < 0)
+			break;
+		QDate d = from_q.value(ix).toDate();
+		QTime t = from_q.value("startTime").toTime();
+		QDateTime dt(d, t);
+		int id = from_q.value("id").toInt();
+		to_q.exec("UPDATE stages SET startDateTime=" QF_SARG(dt.toString(Qt::ISODate)) " WHERE id=" QF_IARG(id));
 	}
 }
 
@@ -522,12 +538,15 @@ void EventPlugin::connectToSqlServer()
 static bool run_sql_script(qf::core::sql::Query &q, const QStringList &sql_lines)
 {
 	qfLogFuncFrame();
+	QVariantMap replacements;
+	replacements["minDbVersion"] = EventPlugin::minDbVersion();
 	for(auto cmd : sql_lines) {
 		if(cmd.isEmpty())
 			continue;
 		if(cmd.startsWith(QLatin1String("--")))
 			continue;
 		qfDebug() << cmd << ';';
+		cmd = qf::core::Utils::replaceCaptions(cmd, replacements);
 		bool ok = q.exec(cmd);
 		if(!ok) {
 			qfInfo() << cmd;
@@ -617,13 +636,16 @@ bool EventPlugin::createEvent(const QString &event_name, const QVariantMap &even
 			QString stage_table_name = "stages";
 			if(connection_type == ConnectionType::SqlServer)
 				stage_table_name = event_id + '.' + stage_table_name;
-			q.prepare("INSERT INTO " + stage_table_name + " (id) VALUES (:id)");
+			QDateTime start_dt = event_config.eventDateTime();
+			q.prepare("INSERT INTO " + stage_table_name + " (id) VALUES (:id, :startDateTime)");
 			for(int i=0; i<stage_count; i++) {
 				q.bindValue(":id", i+1);
+				q.bindValue(":startDateTime", start_dt);
 				ok = q.exec();
 				if(!ok) {
 					break;
 				}
+				start_dt = start_dt.addDays(1);
 			}
 			if(!ok)
 				break;
@@ -756,7 +778,18 @@ bool EventPlugin::openEvent(const QString &_event_name)
 		qfError() << "Invalid connection type:" << static_cast<int>(connection_type);
 	}
 	if(ok) {
-		eventConfig(true);
+		EventConfig *evc = eventConfig(true);
+		if(evc->dbVersion() < minDbVersion()) {
+			qfd::MessageBox::showError(fwk, tr("Event data version (%1) is too low, minimal version is (%2). Use Event/Import to convert event to current version.")
+									   .arg(qf::core::Utils::intToVersionString(evc->dbVersion()))
+									   .arg(qf::core::Utils::intToVersionString(minDbVersion())));
+			closeEvent();
+			return false;
+		}
+		if(evc->dbVersion() > minDbVersion()) {
+			qfd::MessageBox::showError(fwk, tr("Event was created in more recent QuickEvent version (%1) and the application might not work as expected. Download latest QuickEvent is strongly recommended.")
+									   .arg(qf::core::Utils::intToVersionString(evc->dbVersion())));
+		}
 		connection_settings.setEventName(event_name);
 		setEventName(event_name);
 		emit eventOpened(eventName());
@@ -990,6 +1023,9 @@ void EventPlugin::importEvent_qbe()
 			err_str = copy_sql_table(table_name, rec, imp_conn, exp_conn);
 			if(!err_str.isEmpty())
 				break;
+			if(table_name == QLatin1String("stages")) {
+				repairStageStarts(imp_conn, exp_conn);
+			}
 		}
 		if(!err_str.isEmpty())
 			break;

libqf/libqfqmlwidgets/src/framework/datadialogwidget.cpp
@@ -32,21 +32,24 @@ void DataDialogWidget::setDataController(qf::qmlwidgets::DataController *dc)
 	m_dataController = dc;
 }
 
-bool DataDialogWidget::load(const QVariant &id, int mode)
+qf::core::model::DataDocument *DataDialogWidget::dataDocument(bool throw_exc)
 {
 	qf::qmlwidgets::DataController *dc = dataController();
-	if(dc) {
-		auto doc = dc->document(false);
-		if(doc) {
-			//connect(doc, &qfm::DataDocument::saved, this, &DataDialogWidget::dataSaved, Qt::ConnectionType(Qt::QueuedConnection | Qt::UniqueConnection));
-			//qfInfo() << "============" << (bool)c;
-			connect(doc, &qfm::DataDocument::saved, this, &DataDialogWidget::dataSaved, Qt::UniqueConnection);
-			bool ok = doc->load(id, qf::core::model::DataDocument::RecordEditMode(mode));
-			//qfInfo() << "emit ...";
-			if(ok)
-				emit recordEditModeChanged(mode);
-			return ok;
-		}
+	return dc->document(throw_exc);
+}
+
+bool DataDialogWidget::load(const QVariant &id, int mode)
+{
+	core::model::DataDocument *doc = dataDocument(!qf::core::Exception::Throw);
+	if(doc) {
+		//connect(doc, &qfm::DataDocument::saved, this, &DataDialogWidget::dataSaved, Qt::ConnectionType(Qt::QueuedConnection | Qt::UniqueConnection));
+		//qfInfo() << "============" << (bool)c;
+		connect(doc, &qfm::DataDocument::saved, this, &DataDialogWidget::dataSaved, Qt::UniqueConnection);
+		bool ok = doc->load(id, qf::core::model::DataDocument::RecordEditMode(mode));
+		//qfInfo() << "emit ...";
+		if(ok)
+			emit recordEditModeChanged(mode);
+		return ok;
 	}
 	return false;
 }

libqf/libqfqmlwidgets/src/framework/datadialogwidget.h
@@ -24,6 +24,8 @@ public:
 	qf::qmlwidgets::DataController *dataController();
 	void setDataController(qf::qmlwidgets::DataController *dc);
 
+	core::model::DataDocument* dataDocument(bool throw_exc = qf::core::Exception::Throw);
+
 	Q_SLOT virtual bool load(const QVariant &id = QVariant(), int mode = qf::core::model::DataDocument::ModeEdit);
 
 	bool acceptDialogDone(int result) Q_DECL_OVERRIDE;

libqf/libqfqmlwidgets/src/idatawidget.cpp
@@ -19,6 +19,8 @@ IDataWidget::~IDataWidget()
 
 void IDataWidget::loadDataValue(DataController *dc)
 {
+	if(dataId().isEmpty())
+		return;
 	if(dc != m_dataController)
 		m_dataController = dc;
 	qfm::DataDocument *doc = dataDocument(false);

quickevent/app/plugins/qml/Event/qml/DbSchema.qml
@@ -32,8 +32,9 @@ Schema {
 		Table { name: 'stages'
 			fields: [
 				Field { name: 'id'; type: Int {} },
-				Field { name: 'startTime'; type: Time {} },
-				Field { name: 'startDate'; type: Date {} },
+				Field { name: 'startDateTime'; type: DateTime {} },
+				//Field { name: 'startTime'; type: Time {} },
+				//Field { name: 'startDate'; type: Date {} },
 				Field { name: 'drawingConfig'; type: String {} }
 			]
 			indexes: [
@@ -374,7 +375,7 @@ Schema {
 		Insert {
 			table: config
 			rows: [
-				['db.version', qsTr('Data version'), '10004', 'int']
+				['db.version', qsTr('Data version'), '{{minDbVersion}}', 'int']
 				/*
 				['event.stageCount', qsTr('Stage count'), '0', 'int'],
 				['event.name', qsTr('Event name'), '', 'QString'],

quickevent/app/plugins/qml/Event/src/Event/eventconfig.cpp
@@ -184,6 +184,26 @@ QString EventConfig::mainReferee() const
 {
 	return value(QStringLiteral("event.mainReferee")).toString();
 }
+
+QDateTime EventConfig::eventDateTime() const
+{
+	QDateTime dt;
+	QVariant v = value(QStringLiteral("event.dateTime"));
+	if(v.isValid()) {
+		dt = v.toDateTime();
+	}
+	else {
+		QVariant v = value(QStringLiteral("event.date"));
+		dt.setDate(v.toDate());
+		dt = dt.addSecs(10 * 60 * 60); // start at 10:00 if not specified
+	}
+	return dt;
+}
+
+int EventConfig::dbVersion() const
+{
+	return value(QStringLiteral("db.version")).toInt();
+}
 /*
 const QSet<QString> &EventConfig::knownKeys()
 {

quickevent/app/plugins/qml/Event/src/Event/eventconfig.h
@@ -6,6 +6,7 @@
 #include <QObject>
 #include <QVariantMap>
 #include <QSet>
+#include <QDateTime>
 
 namespace Event {
 
@@ -32,10 +33,9 @@ public slots:
 	int handicapLength() const;
 	QString director() const;
 	QString mainReferee() const;
-	//QString eventName() const;
-	//void setEventName(const QString &n);
+	QDateTime eventDateTime() const;
+	int dbVersion() const;
 private:
-	//static const QSet<QString>& knownKeys();
 	void save_helper(QVariantMap &ret, const QString &current_path, const QVariant &val);
 	QVariantMap setValue_helper(const QVariantMap &m, const QStringList &path, const QVariant &val);
 private:

quickevent/app/plugins/qml/Event/src/Event/eventplugin.h
@@ -15,6 +15,7 @@ namespace qf {
 namespace core {
 namespace sql {
 class Query;
+class Connection;
 }
 }
 namespace qmlwidgets {
@@ -90,6 +91,7 @@ public:
 	Q_INVOKABLE QString classNameById(int class_id);
 
 	DbSchema dbSchema();
+	static int minDbVersion();
 public:
 	// event wide signals
 	//Q_SIGNAL void editStartListRequest(int stage_id, int class_id, int competitor_id);
@@ -110,6 +112,7 @@ private:
 	Q_SLOT void onDbEvent(const QString & name, QSqlDriver::NotificationSource source, const QVariant & payload);
 
 	//bool runSqlScript(qf::core::sql::Query &q, const QStringList &sql_lines);
+	void repairStageStarts(const qf::core::sql::Connection &from_conn, const qf::core::sql::Connection &to_conn);
 private:
 	qf::qmlwidgets::Action *m_actConnectDb = nullptr;
 	qf::qmlwidgets::Action *m_actEvent = nullptr;

commit 47c40023e60bcc80493506ec572e552a50c24bd4

eventplugin: Fix SQL statement
Commit 78865b742dd71 broke event setup sequence which manifested itself with
following messages in application debugging output:
  88<WARN>[query.cpp:30] "2 values for 1 columns Unable to execute statement"
  89<WARN>[query.cpp:60] " Parameter count mismatch"

This commit fixes the issue.

Signed-off-by: Libor Pechacek <lpechacek@gmx.com>

quickevent/app/plugins/qml/Event/src/Event/eventplugin.cpp
@@ -640,7 +640,8 @@ bool EventPlugin::createEvent(const QString &event_name, const QVariantMap &even
 			if(connection_type == ConnectionType::SqlServer)
 				stage_table_name = event_id + '.' + stage_table_name;
 			QDateTime start_dt = event_config.eventDateTime();
-			q.prepare("INSERT INTO " + stage_table_name + " (id) VALUES (:id, :startDateTime)");
+			// FIXME: handle SQL errors here and below in q.exec()
+			q.prepare("INSERT INTO " + stage_table_name + " (id, startDateTime) VALUES (:id, :startDateTime)");
 			for(int i=0; i<stage_count; i++) {
 				q.bindValue(":id", i+1);
 				q.bindValue(":startDateTime", start_dt);