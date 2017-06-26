ed55b94368edd871d2b3f9097dd414edf3bdf77f
Fanda Vacek <fanda.vacek@gmail.com>
2017-06-26 23:59:27 +0200

    ob-radio cleanup

--- tools/ob-radio/supervisor.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import time
-
-if __name__ == "__main__":
-	while True:
-		ret = subprocess.call(['python', 'sipunchselect.py'])
-		#print "Exit code: ", ret
-		time.sleep(3)
--- tools/ob-radio/supervisor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-while true ; do
-	python sipunchselect.py 
-	sleep 2
-done
