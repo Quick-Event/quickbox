cb14f47f9e892bb6d8f148f112c3529a349db2f3
Fanda Vacek <fanda.vacek@gmail.com>
2018-05-21 20:32:03 +0200

    QE 1.1.21

--- quickevent/app/src/mainwindow.cpp
+++ quickevent/app/src/mainwindow.cpp
@@ -26,7 +26,7 @@ MainWindow::~MainWindow()
 
 QString MainWindow::versionString() const
 {
-	static QString s = QLatin1String("1.1.20");
+	static QString s = QLatin1String("1.1.21");
 	return s;
 }
 
