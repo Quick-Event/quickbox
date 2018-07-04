54744f351df0dce473884cbd09176fd2f67a3245
Fanda Vacek <fanda.vacek@gmail.com>
2018-07-04 22:37:28 +0200

    remove debug symbols from linux installed files

--- devutils/lctools/fanda-env.sh
+++ devutils/lctools/fanda-env.sh
@@ -5,7 +5,7 @@ PROJ_NAME=quickbox
 FANDA_HOME=/home/fanda
 
 PROJ_HOME=$FANDA_HOME/proj/${PROJ_NAME}
-QT_HOME=$FANDA_HOME/programs/qt5/5.9.1/gcc_64/bin
+QT_HOME=$FANDA_HOME/programs/qt5/5.11.1/gcc_64/bin
 TS_HOME=$FANDA_HOME/t/${PROJ_NAME}-ts
 PROJ_BUILD=$FANDA_HOME/t/_distro/_build/gcc_64
 QM_DEST_DIR=$PROJ_BUILD/bin/translations
