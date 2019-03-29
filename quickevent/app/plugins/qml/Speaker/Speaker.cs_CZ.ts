3eb0c8335ecd8dc0c6dab6d844cc8e42f2e1e9eb
Lukáš Paukert <paukert.lukas@gmail.com>
2019-03-29 14:25:50 +0000

    Translated using Weblate (Czech)
    Currently translated at 88.9% (16 of 18 strings)
    
    Translation: QuickBox/Plugin: Speaker
    Translate-URL: https://hosted.weblate.org/projects/quickbox/plugin-speaker/cs/

--- quickevent/app/plugins/qml/Speaker/Speaker.cs_CZ.ts
+++ quickevent/app/plugins/qml/Speaker/Speaker.cs_CZ.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="src/codeclassresultswidget.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Formulář</translation>
     </message>
     <message>
         <location filename="src/codeclassresultswidget.ui" line="37"/>
         <source>Class</source>
-        <translation type="unfinished"></translation>
+        <translation>Kategorie</translation>
     </message>
     <message>
         <location filename="src/codeclassresultswidget.ui" line="47"/>
         <source>Code</source>
-        <translation type="unfinished"></translation>
+        <translation>Kontrola</translation>
     </message>
     <message>
         <location filename="src/codeclassresultswidget.cpp" line="47"/>
         <source>Competitor</source>
-        <translation type="unfinished"></translation>
+        <translation>Závodník</translation>
     </message>
     <message>
         <location filename="src/codeclassresultswidget.cpp" line="48"/>
         <source>Reg</source>
-        <translation type="unfinished"></translation>
+        <translation>Reg</translation>
     </message>
     <message>
         <location filename="src/codeclassresultswidget.cpp" line="49"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Čas</translation>
     </message>
     <message>
         <location filename="src/codeclassresultswidget.cpp" line="159"/>
         <source>Results</source>
-        <translation type="unfinished"></translation>
+        <translation>Výsledky</translation>
     </message>
     <message>
         <location filename="src/codeclassresultswidget.cpp" line="160"/>
         <source>Finish</source>
-        <translation type="unfinished"></translation>
+        <translation>Cíl</translation>
     </message>
     <message>
         <location filename="src/codeclassresultswidget.cpp" line="179"/>
@@ -55,42 +55,42 @@
     <message>
         <location filename="src/speakerwidget.ui" line="20"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Formulář</translation>
     </message>
     <message>
         <location filename="src/speakerwidget.cpp" line="83"/>
         <source>Code</source>
-        <translation type="unfinished"></translation>
+        <translation>Kontrola</translation>
     </message>
     <message>
         <location filename="src/speakerwidget.cpp" line="84"/>
         <source>SI</source>
-        <translation type="unfinished"></translation>
+        <translation>SI</translation>
     </message>
     <message>
         <location filename="src/speakerwidget.cpp" line="85"/>
         <source>Punch time</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Čas ražení</translation>
     </message>
     <message>
         <location filename="src/speakerwidget.cpp" line="86"/>
         <source>Runner time</source>
-        <translation type="unfinished"></translation>
+        <translation>Čas závodníka</translation>
     </message>
     <message>
         <location filename="src/speakerwidget.cpp" line="87"/>
         <source>Class</source>
-        <translation type="unfinished"></translation>
+        <translation>Kategorie</translation>
     </message>
     <message>
         <location filename="src/speakerwidget.cpp" line="88"/>
         <source>Registration</source>
-        <translation type="unfinished"></translation>
+        <translation>Registrace</translation>
     </message>
     <message>
         <location filename="src/speakerwidget.cpp" line="89"/>
         <source>Competitor</source>
-        <translation type="unfinished"></translation>
+        <translation>Závodník</translation>
     </message>
 </context>
 <context>
@@ -98,7 +98,7 @@
     <message>
         <location filename="src/thispartwidget.cpp" line="12"/>
         <source>&amp;Speaker</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Komentátor</translation>
     </message>
 </context>
 </TS>
