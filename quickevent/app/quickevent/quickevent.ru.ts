67b7fb7cf032f4fcaa85b2bcc1a77f4f1b4bebe5
Nikolay Korotkiy <sikmir@gmail.com>
2019-04-28 12:33:47 +0000

    Added translation using Weblate (Russian)

---
+++ quickevent/app/quickevent/quickevent.ru.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>AppCliOptions</name>
+    <message>
+        <source>Application locale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Application profile, see: https://github.com/fvacek/quickbox/wiki/Application-profiles</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoggerWidget</name>
+    <message>
+        <source>&lt;empty&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Quick Event ver. %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Application log</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
