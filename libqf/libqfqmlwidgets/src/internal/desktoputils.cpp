f9a2e363eabc7cc8c0dd286bcfd7295fec920ac0
Fanda Vacek <fanda.vacek@gmail.com>
2019-09-11 16:12:06 +0200

    app crash fixed

--- libqf/libqfqmlwidgets/src/internal/desktoputils.cpp
+++ libqf/libqfqmlwidgets/src/internal/desktoputils.cpp
@@ -13,12 +13,12 @@ QRect DesktopUtils::moveRectToVisibleDesktopScreen(const QRect &r)
 	QRect ret = r;
 #if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
 	QScreen *scr = QApplication::screenAt(ret.topLeft());
-	QRect screen_rect = scr->geometry();
+	QRect screen_rect = scr? scr->geometry(): QRect();
 #else
 	QDesktopWidget *dw = QApplication::desktop();
 	QRect screen_rect = dw->screenGeometry(ret.topLeft());
 #endif
-	if(!screen_rect.contains(ret.topLeft()))
+	if(screen_rect.isValid() && !screen_rect.contains(ret.topLeft()))
 		ret.moveTopLeft(screen_rect.topLeft());
 	return ret;
 }
