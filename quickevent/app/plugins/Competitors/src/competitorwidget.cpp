0a4b54ad12a42f103717e991b6a297dc8174fb6a
Otakar Hirs <ota@localhost.localdomain>
2020-03-19 22:15:06 +0100

    fix class is set check

--- quickevent/app/plugins/Competitors/src/competitorwidget.cpp
+++ quickevent/app/plugins/Competitors/src/competitorwidget.cpp
@@ -350,7 +350,8 @@ void CompetitorWidget::onRegistrationSelected(const QVariantMap &values)
 		qfDebug() << "\t" << s << "->" << values.value(s);
 		doc->setValue(s, values.value(s));
 	}
-	if(!doc->isDirty(QStringLiteral("classId"))) {
+	// if no class is set, guess class from registration
+	if(ui->cbxClass->currentText().isEmpty()) {
 		QString class_name_prefix = guessClassFromRegistration(values.value("registration").toString());
 		if(!class_name_prefix.isEmpty()) {
 			for (int i = 0; i < ui->cbxClass->count(); ++i) {
