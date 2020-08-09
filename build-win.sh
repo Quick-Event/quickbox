30568d7b0b43129e2929331fba6a2beae758a9a1
Fanda Vacek <vacek@elektroline.cz>
2020-08-09 11:43:47 +0200

    Update build-win.sh

--- build-win.sh
+++ build-win.sh
@@ -1,7 +1,7 @@
 echo "making quickevent" $WORKSPACE
 PATH=/c/Qt/Tools/mingw810_64/bin:$PATH
 c:/Qt/5.15.0/mingw81_64/bin/qmake.exe DEFINES+=GIT_COMMIT=${CI_COMMIT_SHA} DEFINES+=GIT_BRANCH=${CI_COMMIT_REF_SLUG} MINGW_DIR=c:/Qt/Tools/mingw730_64 -r CONFIG+=release CONFIG+=force_debug_info CONFIG+=separate_debug_info quickbox.pro || exit 2
-c:/Qt/Tools/mingw810_64/bin/mingw32-make.exe -j1 || exit 2
+c:/Qt/Tools/mingw810_64/bin/mingw32-make.exe -j2 || exit 2
 
 #[ -d bin/translations ] || mkdir bin/translations  || exit 2
 
