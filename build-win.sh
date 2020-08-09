517c34f8cfa769d4161446bc5dfdfc27ed374b51
Fanda Vacek <fanda.vacek@gmail.com>
2020-08-09 22:05:19 +0200

    call lrelease in win build

--- build-win.sh
+++ build-win.sh
@@ -1,9 +1,16 @@
 echo "making quickevent" $WORKSPACE
-PATH=/c/Qt/Tools/mingw810_64/bin:$PATH
-c:/Qt/5.15.0/mingw81_64/bin/qmake.exe DEFINES+=GIT_COMMIT=${CI_COMMIT_SHA} DEFINES+=GIT_BRANCH=${CI_COMMIT_REF_SLUG} -r CONFIG+=release CONFIG+=force_debug_info CONFIG+=separate_debug_info quickbox.pro || exit 2
-c:/Qt/Tools/mingw810_64/bin/mingw32-make.exe -j2 || exit 2
+QT_DIR=/c/Qt/5.15.0/mingw81_64
+MINGW_DIR=/c/Qt/Tools/mingw810_64
+PATH=$MINGW_DIR/bin:$PATH
+$QT_DIR/bin/qmake DEFINES+=GIT_COMMIT=${CI_COMMIT_SHA} DEFINES+=GIT_BRANCH=${CI_COMMIT_REF_SLUG} -r CONFIG+=release CONFIG+=force_debug_info CONFIG+=separate_debug_info quickbox.pro || exit 2
+$MINGW_DIR/bin/mingw32-make.exe -j2 || exit 2
 
-#[ -d bin/translations ] || mkdir bin/translations  || exit 2
+TRANS_DIR=bin/translations
+mkdir -p $TRANS_DIR
+for tsfile in `find . -name "*.ts"` ; do
+	qmfile=`basename "${tsfile%.*}.qm"`
+	$QT_DIR/bin/lrelease $tsfile -qm $TRANS_DIR/$qmfile
+done
 
 QUICKEVENT_VERSION=`grep APP_VERSION quickevent/app/quickevent/src/appversion.h | cut -d\" -f2`
 
