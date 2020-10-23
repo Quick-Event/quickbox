8278c15d938d1b40573e3e5b8e666d618082ac2e
vtararin <viacheslav.tararin@gmail.com>
2020-10-23 12:44:25 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 90.9% (20 of 22 strings)
    
    Translation: QuickBox/Plugin: libquickeventcore
    Translate-URL: https://hosted.weblate.org/projects/quickbox/plugin-examplecppqml/uk/

--- libquickevent/libquickeventcore/libquickeventcore.uk_UA.ts
+++ libquickevent/libquickeventcore/libquickeventcore.uk_UA.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="src/exporters/htmlfileexporter.cpp" line="118"/>
         <source>E%1 %2</source>
-        <translation type="unfinished"></translation>
+        <translation>E%1 %2</translation>
     </message>
 </context>
 <context>
@@ -14,22 +14,22 @@
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="19"/>
         <source>Results</source>
-        <translation type="unfinished"></translation>
+        <translation>Результати</translation>
     </message>
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="41"/>
         <source>E%1 %2 %3</source>
-        <translation type="unfinished"></translation>
+        <translation>E%1 %2 %3</translation>
     </message>
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="45"/>
         <source> length: </source>
-        <translation type="unfinished"></translation>
+        <translation> довжина: </translation>
     </message>
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="45"/>
         <source>climb: </source>
-        <translation type="unfinished"></translation>
+        <translation>набір: </translation>
     </message>
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="63"/>
@@ -39,22 +39,22 @@
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="64"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Назва</translation>
     </message>
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="65"/>
         <source>Registration</source>
-        <translation type="unfinished"></translation>
+        <translation>Реєстрація</translation>
     </message>
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="66"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Час</translation>
     </message>
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="67"/>
         <source>Status</source>
-        <translation type="unfinished"></translation>
+        <translation>Статус</translation>
     </message>
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="80"/>
@@ -64,12 +64,12 @@
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="84"/>
         <source>DISQ</source>
-        <translation type="unfinished"></translation>
+        <translation>Дискв</translation>
     </message>
     <message>
         <location filename="src/exporters/stageresultshtmlexporter.cpp" line="109"/>
         <source>Results %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Результати</translation>
     </message>
 </context>
 <context>
@@ -77,47 +77,47 @@
     <message>
         <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="19"/>
         <source>Start List</source>
-        <translation type="unfinished"></translation>
+        <translation>Стартовий протокол</translation>
     </message>
     <message>
         <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="41"/>
         <source>E%1 %2 Start list</source>
-        <translation type="unfinished"></translation>
+        <translation>E%1 %2 Стартовий протокол</translation>
     </message>
     <message>
         <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="45"/>
         <source> length: </source>
-        <translation type="unfinished"></translation>
+        <translation> довжина: </translation>
     </message>
     <message>
         <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="45"/>
         <source>climb: </source>
-        <translation type="unfinished"></translation>
+        <translation>набір: </translation>
     </message>
     <message>
         <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="63"/>
         <source>Start</source>
-        <translation type="unfinished"></translation>
+        <translation>Старт</translation>
     </message>
     <message>
         <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="64"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Назва</translation>
     </message>
     <message>
         <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="65"/>
         <source>Registration</source>
-        <translation type="unfinished"></translation>
+        <translation>Реєстрація</translation>
     </message>
     <message>
         <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="66"/>
         <source>SI</source>
-        <translation type="unfinished"></translation>
+        <translation>ЧІП</translation>
     </message>
     <message>
         <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="89"/>
         <source>%1 %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 %2</translation>
     </message>
 </context>
 </TS>
