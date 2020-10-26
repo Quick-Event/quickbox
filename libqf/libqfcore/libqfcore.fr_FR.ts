a6f64ccf343f049f01340adb9e08bd26924ac4f9
Nathan <bonnemainsnathan@gmail.com>
2020-10-26 12:03:44 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (27 of 27 strings)
    
    Translation: QuickBox/libqf: core
    Translate-URL: https://hosted.weblate.org/projects/quickbox/libqf-core/fr/

--- libqf/libqfcore/libqfcore.fr_FR.ts
+++ libqf/libqfcore/libqfcore.fr_FR.ts
@@ -7,7 +7,7 @@
         <location filename="src/sql/catalog.cpp" line="32"/>
         <location filename="src/sql/catalog.cpp" line="60"/>
         <source>Error getting the sequence nextval(&apos;%1&apos;)</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur lors de la récupération de la séquence nextval(&apos;%1&apos;)</translation>
     </message>
 </context>
 <context>
@@ -15,7 +15,7 @@
     <message>
         <location filename="src/core/logdevice.cpp" line="142"/>
         <source>Cannot find log device by object name &apos;%1&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible de trouver le périphérique de journal par non d&apos;objet « %1 »</translation>
     </message>
 </context>
 <context>
@@ -23,42 +23,42 @@
     <message>
         <location filename="src/model/logtablemodel.cpp" line="49"/>
         <source>Category</source>
-        <translation type="unfinished"></translation>
+        <translation>Catégorie</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="51"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="53"/>
         <source>Line</source>
-        <translation type="unfinished"></translation>
+        <translation>Ligne</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="55"/>
         <source>Severity</source>
-        <translation type="unfinished"></translation>
+        <translation>Sévérité</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="57"/>
         <source>Time stamp</source>
-        <translation type="unfinished"></translation>
+        <translation>Horodatage</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="59"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Message</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="61"/>
         <source>Function</source>
-        <translation type="unfinished"></translation>
+        <translation>Fonction</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="63"/>
         <source>Data</source>
-        <translation type="unfinished"></translation>
+        <translation>Données</translation>
     </message>
 </context>
 <context>
@@ -67,13 +67,15 @@
         <location filename="src/model/sqltablemodel.cpp" line="230"/>
         <source>numRowsAffected() = %1, should be 1
 %2</source>
-        <translation type="unfinished"></translation>
+        <translation>numRowsAffected() = %1, devrait être 1
+%2</translation>
     </message>
     <message>
         <location filename="src/model/sqltablemodel.cpp" line="256"/>
         <source>Error executing query: %1
  %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur lors de l&apos;exécution de la requête : %1
+ %2</translation>
     </message>
 </context>
 <context>
@@ -84,21 +86,21 @@
         <location filename="src/model/tablemodel.cpp" line="459"/>
         <location filename="src/model/tablemodel.cpp" line="563"/>
         <source>Cannot find table field index for column index: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible de trouver l&apos;indice de champ de table pour l&apos;indice de colonne : %1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="424"/>
         <location filename="src/model/tablemodel.cpp" line="455"/>
         <location filename="src/model/tablemodel.cpp" line="559"/>
         <source>Invalid table row: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ligne de table invalide : %1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="431"/>
         <location filename="src/model/tablemodel.cpp" line="462"/>
         <location filename="src/model/tablemodel.cpp" line="566"/>
         <source>Invalid table field index: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Indice de champ de table invalide : %1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="447"/>
@@ -107,39 +109,39 @@
         <location filename="src/model/tablemodel.cpp" line="551"/>
         <location filename="src/model/tablemodel.cpp" line="577"/>
         <source>Cannot find column index for name: &apos;%1&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible de trouver l&apos;indice de colonne pour le nom : « %1 »</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="478"/>
         <source>%2 Cannot find table field for column index: %1 (row index: %3)</source>
-        <translation type="unfinished"></translation>
+        <translation>%2 Impossible de trouver le champ de table pour l&apos;indice de colonne : %1 (indice de ligne : %3)</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="503"/>
         <location filename="src/model/tablemodel.cpp" line="538"/>
         <source>Cannot find table field for column index: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible de trouver le champ de table pour l&apos;indice de colonne : %1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="622"/>
         <location filename="src/model/tablemodel.cpp" line="683"/>
         <source>Invalid column index: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Indice de colonne invalide : %1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="631"/>
         <source>Invalid field for column index: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Champ invalide pour l&apos;indice de colonne : %1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="672"/>
         <source>Column named &apos;%1&apos; not found in column list. Existing columns: [%2]</source>
-        <translation type="unfinished"></translation>
+        <translation>La colonne « %1 » n&apos;a pas été trouvée dans la liste des colonnes. Colonnes existantes : [%2]</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="702"/>
         <source>Cannot find table field for column index: %1, table index: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible de trouver le champ de table pour l&apos;indice de colonne : %1, et l&apos;indice de table : %2</translation>
     </message>
 </context>
 <context>
@@ -147,7 +149,7 @@
     <message>
         <location filename="src/sql/dbfsdriver.cpp" line="109"/>
         <source>Connection &apos;%1&apos; is not open!</source>
-        <translation type="unfinished"></translation>
+        <translation>La connexion « %1 » n&apos;est pas ouverte !</translation>
     </message>
 </context>
 <context>
@@ -156,7 +158,7 @@
         <location filename="src/sql/catalog.cpp" line="300"/>
         <location filename="src/sql/catalog.cpp" line="318"/>
         <source>Found info for nonexisting field &apos;%1&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Informations trouvées pour le champ « %1 » inexistant</translation>
     </message>
 </context>
 <context>
@@ -164,17 +166,17 @@
     <message>
         <location filename="src/utils/clioptions.cpp" line="99"/>
         <source>Abort application on exception</source>
-        <translation type="unfinished"></translation>
+        <translation>Fermer l&apos;application en cas d&apos;exception</translation>
     </message>
     <message>
         <location filename="src/utils/clioptions.cpp" line="100"/>
         <source>Print help</source>
-        <translation type="unfinished"></translation>
+        <translation>Afficher l&apos;aide</translation>
     </message>
     <message>
         <location filename="src/utils/clioptions.cpp" line="101"/>
         <source>Config name, it is loaded from {app-name}[.conf] if file exists in {config-path}</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom de la configuration, elle est chargée à partir du fichier {app-name}[.conf] si le fichier existe à {config-path}</translation>
     </message>
 </context>
 </TS>
