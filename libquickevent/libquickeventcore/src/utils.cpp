cce4860d1fe8ce6b7fbeb52e3c481f2befaf5b02
Otakar Hirš <otakar.hirs@gmail.com>
2021-06-14 13:03:07 +0200

    add comment

--- libquickevent/libquickeventcore/src/utils.cpp
+++ libquickevent/libquickeventcore/src/utils.cpp
@@ -7,6 +7,7 @@ namespace core {
 
  QString Utils::dateTimeToIsoStringWithUtcOffset(QDateTime dt)
 {
+	// offset is missing if not explicitly set, see https://bugreports.qt.io/browse/QTBUG-26161?focusedCommentId=554227
 	dt.setOffsetFromUtc(dt.offsetFromUtc());
 	return dt.toString(Qt::ISODate);
 }
