d643e44bc472f9b22b8af1ae12e612b209e0f748
Allan Nordhøy <epost@anotheragency.no>
2021-07-15 17:09:29 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 57.1% (4 of 7 strings)
    
    Translation: QuickBox/quickshow
    Translate-URL: https://hosted.weblate.org/projects/quickbox/quickshow/nb_NO/

--- quickshow/quickshow-nb_NO.ts
+++ quickshow/quickshow-nb_NO.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="src/cellrenderer.cpp" line="77"/>
         <source>length: {{length}} climb: {{climb}}</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">lengde: {{length}} klatring: {{climb}}</translation>
     </message>
 </context>
 <context>
@@ -14,27 +14,27 @@
     <message>
         <location filename="src/mainwindow.ui" line="14"/>
         <source>MainWindow</source>
-        <translation type="unfinished"></translation>
+        <translation>Hovedvindu</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="26"/>
         <source>Toggle full screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Veksle fullskjermsvisning</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="39"/>
         <source>Increase font size</source>
-        <translation type="unfinished"></translation>
+        <translation>Øk skriftstørrelse</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="52"/>
         <source>Decrease font size</source>
-        <translation type="unfinished"></translation>
+        <translation>Forminsk skriftstørrelse</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="79"/>
         <source>E%1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">E%1</translation>
     </message>
 </context>
 <context>
