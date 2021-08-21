1ad20860bd5c0685ac3642b44c6e94b2cad666f8
Tymofii Lytvynenko <till.svit@gmail.com>
2021-08-21 18:15:13 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: QuickBox/libqf: core
    Translate-URL: https://hosted.weblate.org/projects/quickbox/libqf-core/uk/

--- libqf/libqfcore/libqfcore-uk_UA.ts
+++ libqf/libqfcore/libqfcore-uk_UA.ts
@@ -7,7 +7,7 @@
         <location filename="src/sql/catalog.cpp" line="33"/>
         <location filename="src/sql/catalog.cpp" line="61"/>
         <source>Error getting the sequence nextval(&apos;%1&apos;)</source>
-        <translation>Error getting the sequence nextval(&apos;%1&apos;)</translation>
+        <translation>Помилка при отриманні послідовності nextval (&apos;%1&apos;)</translation>
     </message>
 </context>
 <context>
@@ -15,42 +15,42 @@
     <message>
         <location filename="src/model/logtablemodel.cpp" line="49"/>
         <source>Category</source>
-        <translation>Category</translation>
+        <translation>Категорія</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="51"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>Файл</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="53"/>
         <source>Line</source>
-        <translation type="unfinished"></translation>
+        <translation>Рядок</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="55"/>
         <source>Severity</source>
-        <translation type="unfinished"></translation>
+        <translation>Важливість</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="57"/>
         <source>Time stamp</source>
-        <translation type="unfinished"></translation>
+        <translation>Позначка часу</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="59"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Повідомлення</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="61"/>
         <source>Function</source>
-        <translation type="unfinished"></translation>
+        <translation>Функція</translation>
     </message>
     <message>
         <location filename="src/model/logtablemodel.cpp" line="63"/>
         <source>Data</source>
-        <translation type="unfinished"></translation>
+        <translation>Дані</translation>
     </message>
 </context>
 <context>
@@ -59,13 +59,15 @@
         <location filename="src/model/sqltablemodel.cpp" line="231"/>
         <source>numRowsAffected() = %1, should be 1
 %2</source>
-        <translation type="unfinished"></translation>
+        <translation>numRowsAffected() = %1, повинно бути 1
+%2</translation>
     </message>
     <message>
         <location filename="src/model/sqltablemodel.cpp" line="257"/>
         <source>Error executing query: %1
  %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Помилка виконання запиту: %1
+ %2</translation>
     </message>
 </context>
 <context>
@@ -76,21 +78,21 @@
         <location filename="src/model/tablemodel.cpp" line="459"/>
         <location filename="src/model/tablemodel.cpp" line="563"/>
         <source>Cannot find table field index for column index: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Неможливо знайти індекс поля таблиці для індексу стовпця: %1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="424"/>
         <location filename="src/model/tablemodel.cpp" line="455"/>
         <location filename="src/model/tablemodel.cpp" line="559"/>
         <source>Invalid table row: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Неприпустимий рядок таблиці:%1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="431"/>
         <location filename="src/model/tablemodel.cpp" line="462"/>
         <location filename="src/model/tablemodel.cpp" line="566"/>
         <source>Invalid table field index: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Неприпустимий індекс поля таблиці: %1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="447"/>
@@ -99,39 +101,39 @@
         <location filename="src/model/tablemodel.cpp" line="551"/>
         <location filename="src/model/tablemodel.cpp" line="577"/>
         <source>Cannot find column index for name: &apos;%1&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Неможливо знайти індекс стовпця для імені: &apos;%1&apos;</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="478"/>
         <source>%2 Cannot find table field for column index: %1 (row index: %3)</source>
-        <translation type="unfinished"></translation>
+        <translation>%2 Не вдається знайти поле таблиці для індексу стовпця: %1 (індекс рядка: %3)</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="503"/>
         <location filename="src/model/tablemodel.cpp" line="538"/>
         <source>Cannot find table field for column index: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдається знайти поле таблиці для індексу стовпця: %1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="622"/>
         <location filename="src/model/tablemodel.cpp" line="683"/>
         <source>Invalid column index: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Недійсний індекс стовпця: %1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="631"/>
         <source>Invalid field for column index: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Недійсне поле для індексу стовпця: %1</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="672"/>
         <source>Column named &apos;%1&apos; not found in column list. Existing columns: [%2]</source>
-        <translation type="unfinished"></translation>
+        <translation>Стовпець з назвою &quot;%1&quot; не знайдено у списку стовпців. Наявні стовпці: [%2]</translation>
     </message>
     <message>
         <location filename="src/model/tablemodel.cpp" line="702"/>
         <source>Cannot find table field for column index: %1, table index: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдається знайти поле таблиці для індексу стовпця: %1, індекс таблиці: %2</translation>
     </message>
 </context>
 <context>
@@ -139,7 +141,7 @@
     <message>
         <location filename="src/sql/dbfsdriver.cpp" line="109"/>
         <source>Connection &apos;%1&apos; is not open!</source>
-        <translation type="unfinished"></translation>
+        <translation>З&apos;єднання &quot;%1&quot; не відкрите!</translation>
     </message>
 </context>
 <context>
@@ -148,7 +150,7 @@
         <location filename="src/sql/catalog.cpp" line="301"/>
         <location filename="src/sql/catalog.cpp" line="319"/>
         <source>Found info for nonexisting field &apos;%1&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Знайдено відомості для відсутнього поля &quot;%1&quot;</translation>
     </message>
 </context>
 <context>
@@ -156,17 +158,17 @@
     <message>
         <location filename="src/utils/clioptions.cpp" line="99"/>
         <source>Abort application on exception</source>
-        <translation type="unfinished"></translation>
+        <translation>Примусово перервати заявку на виключення</translation>
     </message>
     <message>
         <location filename="src/utils/clioptions.cpp" line="100"/>
         <source>Print help</source>
-        <translation type="unfinished"></translation>
+        <translation>Друк довідки</translation>
     </message>
     <message>
         <location filename="src/utils/clioptions.cpp" line="101"/>
         <source>Config name, it is loaded from {app-name}[.conf] if file exists in {config-path}</source>
-        <translation type="unfinished"></translation>
+        <translation>Назва конфігурації завантажується з {app-name} [. Conf], якщо файл існує в {config-path}</translation>
     </message>
 </context>
 </TS>
