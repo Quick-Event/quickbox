1fae927fffd2670e279d193f32cf8e58abfb6f9a
Tymofii Lytvynenko <till.svit@gmail.com>
2021-08-21 17:53:50 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: QuickBox/libsiut
    Translate-URL: https://hosted.weblate.org/projects/quickbox/libsiut/uk/

--- libsiut/libsiut-uk_UA.ts
+++ libsiut/libsiut-uk_UA.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="src/sicard.cpp" line="42"/>
         <source>stationNumber: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>номерСтанції: %1</translation>
     </message>
     <message>
         <location filename="src/sicard.cpp" line="43"/>
         <source>cardNumber: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>номерКартки: %1</translation>
     </message>
     <message>
         <location filename="src/sicard.cpp" line="47"/>
         <source>check: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>перевірка: %1</translation>
     </message>
     <message>
         <location filename="src/sicard.cpp" line="48"/>
         <source>start: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>початок: %1</translation>
     </message>
     <message>
         <location filename="src/sicard.cpp" line="49"/>
         <source>finish: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>закінчення: %1</translation>
     </message>
 </context>
 <context>
@@ -34,27 +34,27 @@
     <message>
         <location filename="src/device/commport.cpp" line="42"/>
         <source>Available ports: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Доступні порти: %1</translation>
     </message>
     <message>
         <location filename="src/device/commport.cpp" line="50"/>
         <source>Connecting to %1 - baudrate: %2, data bits: %3, parity: %4, stop bits: %5</source>
-        <translation type="unfinished"></translation>
+        <translation>Під&apos;єднання до %1 - швидкість передачі даних: %2, біти даних: %3, парність: %4, стоп-біти: %5</translation>
     </message>
     <message>
         <location filename="src/device/commport.cpp" line="60"/>
         <source>%1 connected OK</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 під&apos;єднано ВДАЛО</translation>
     </message>
     <message>
         <location filename="src/device/commport.cpp" line="64"/>
         <source>%1 connect ERROR: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 ПОМИЛКА під&apos;єднання: %2</translation>
     </message>
     <message>
         <location filename="src/device/commport.cpp" line="74"/>
         <source>%1 closed</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 закрито</translation>
     </message>
 </context>
 <context>
@@ -62,27 +62,27 @@
     <message>
         <location filename="src/device/sidevicedriver.cpp" line="119"/>
         <source>Garbage received, stripping %1 characters from beginning of buffer</source>
-        <translation type="unfinished"></translation>
+        <translation>Отримано Garbage, видалення %1 символів від початку буфера</translation>
     </message>
     <message>
         <location filename="src/device/sidevicedriver.cpp" line="136"/>
         <source>NAK received</source>
-        <translation type="unfinished"></translation>
+        <translation>НАК отримано</translation>
     </message>
     <message>
         <location filename="src/device/sidevicedriver.cpp" line="142"/>
         <source>Legacy protocol is not supported, switch station to extended one.</source>
-        <translation type="unfinished"></translation>
+        <translation>Старий протокол не підтримується, перемикніть станцію на розширену.</translation>
     </message>
     <message>
         <location filename="src/device/sidevicedriver.cpp" line="149"/>
         <source>Valid message shall end with ETX or NAK, throwing data away</source>
-        <translation type="unfinished"></translation>
+        <translation>Дійсне повідомлення закінчується ETX або NAK, викидаючи дані</translation>
     </message>
     <message>
         <location filename="src/device/sidevicedriver.cpp" line="165"/>
         <source>SIDeviceDriver::sendCommand() - ERROR Sending of EXT commands only is supported for sending.</source>
-        <translation type="unfinished"></translation>
+        <translation>SIDeviceDriver::sendCommand() - ПОМИЛКА: надсилання EXT команд підтримується тільки для надсилань</translation>
     </message>
 </context>
 <context>
@@ -90,7 +90,7 @@
     <message>
         <location filename="src/device/sitask.cpp" line="24"/>
         <source>SiCommand timeout after %1 sec.</source>
-        <translation type="unfinished"></translation>
+        <translation>Час очікування SiCommand через %1 сек.</translation>
     </message>
 </context>
 </TS>
