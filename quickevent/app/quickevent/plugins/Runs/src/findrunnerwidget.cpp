c09cab05998e2e3f0f0ba8e0cc44d556349de268
Otakar Hirš <otakar.hirs@gmail.com>
2022-01-28 13:11:57 +0100

    hide UseInNextStages if event has only one stage

--- quickevent/app/quickevent/plugins/Runs/src/findrunnerwidget.cpp
+++ quickevent/app/quickevent/plugins/Runs/src/findrunnerwidget.cpp
@@ -7,11 +7,13 @@
 #include <qf/core/model/sqltablemodel.h>
 #include <qf/core/sql/querybuilder.h>
 #include <qf/core/assert.h>
+#include <plugins/Event/src/eventplugin.h>
 
 namespace qfm = qf::core::model;
 namespace qfs = qf::core::sql;
 using qf::qmlwidgets::framework::getPlugin;
 using Runs::RunsPlugin;
+using Event::EventPlugin;
 
 namespace Runs {
 
@@ -25,6 +27,9 @@ FindRunnerWidget::FindRunnerWidget(int stage_id, QWidget *parent)
 	ui->setupUi(this);
 	ui->edFindRunner->setFocus();
 	ui->edFindRunner->setTable(getPlugin<RunsPlugin>()->runnersTable(stage_id));
+	if (getPlugin<EventPlugin>()->stageCount() == 1) {
+		ui->cbxUseSIInNextStages->hide();
+	}
 	//connect(ui->edFindRunner, &FindRunnerEdit::runnerSelected, this, &FindRunnerWidget::onRunnerSelected);
 }
 
