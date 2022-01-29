dbae4b4f3775ede9f4a49e731fe9c4df48929658
Fanda Vacek <fvacek@elektroline.cz>
2022-01-29 11:22:42 +0100

    local reference return warning in necrolog operator<< overloads fixed

--- libqf/libqfcore/src/core/log.h
+++ libqf/libqfcore/src/core/log.h
@@ -42,46 +42,46 @@
 
 #define qfFatal(msg) {qfError() << msg; abort();}
 
-inline NecroLog &operator<<(NecroLog log, const QString &s) { return log.operator<<(s.toStdString()); }
-inline NecroLog &operator<<(NecroLog log, const QDateTime &dt) { return log.operator<<(dt.toString(Qt::ISODateWithMs).toStdString()); }
-inline NecroLog &operator<<(NecroLog log, const QDate &dt) { return log.operator<<(dt.toString(Qt::ISODateWithMs).toStdString()); }
-inline NecroLog &operator<<(NecroLog log, const QTime &dt) { return log.operator<<(dt.toString(Qt::ISODateWithMs).toStdString()); }
-inline NecroLog &operator<<(NecroLog log, const QUrl &url) { return log.operator<<(url.toString().toStdString()); }
-inline NecroLog &operator<<(NecroLog log, const QStringList &sl) {
+inline NecroLog operator<<(NecroLog log, const QString &s) { return log.operator<<(s.toStdString()); }
+inline NecroLog operator<<(NecroLog log, const QDateTime &dt) { return log.operator<<(dt.toString(Qt::ISODateWithMs).toStdString()); }
+inline NecroLog operator<<(NecroLog log, const QDate &dt) { return log.operator<<(dt.toString(Qt::ISODateWithMs).toStdString()); }
+inline NecroLog operator<<(NecroLog log, const QTime &dt) { return log.operator<<(dt.toString(Qt::ISODateWithMs).toStdString()); }
+inline NecroLog operator<<(NecroLog log, const QUrl &url) { return log.operator<<(url.toString().toStdString()); }
+inline NecroLog operator<<(NecroLog log, const QStringList &sl) {
 	QString s = '[' + sl.join(',') + ']';
 	return log.operator<<(s.toStdString());
 }
-inline NecroLog &operator<<(NecroLog log, const QByteArray &ba) {
+inline NecroLog operator<<(NecroLog log, const QByteArray &ba) {
 	QString s = ba.toHex();
 	return log.operator<<(s.toStdString());
 }
-inline NecroLog &operator<<(NecroLog log, const QVariant &v) {
+inline NecroLog operator<<(NecroLog log, const QVariant &v) {
 	QString s = v.toString();
 	return log.operator<<(s.toStdString());
 }
-inline NecroLog &operator<<(NecroLog log, const qf::core::String &s) { return log.operator<<(s.toStdString()); }
-inline NecroLog &operator<<(NecroLog log, const QPoint &p) {
+inline NecroLog operator<<(NecroLog log, const qf::core::String &s) { return log.operator<<(s.toStdString()); }
+inline NecroLog operator<<(NecroLog log, const QPoint &p) {
 	QString s = "QPoint(%1, %2)";
 	return log.operator<<(s.arg(p.x()).arg(p.y()).toStdString());
 }
-inline NecroLog &operator<<(NecroLog log, const QPointF &p) {
+inline NecroLog operator<<(NecroLog log, const QPointF &p) {
 	QString s = "QPoint(%1, %2)";
 	return log.operator<<(s.arg(p.x()).arg(p.y()).toStdString());
 }
-inline NecroLog &operator<<(NecroLog log, const QSize &sz) {
+inline NecroLog operator<<(NecroLog log, const QSize &sz) {
 	QString s = "QSize(%1, %2)";
 	return log.operator<<(s.arg(sz.width()).arg(sz.height()).toStdString());
 }
-inline NecroLog &operator<<(NecroLog log, const QSizeF &sz) {
+inline NecroLog operator<<(NecroLog log, const QSizeF &sz) {
 	QString s = "QSize(%1, %2)";
 	return log.operator<<(s.arg(sz.width()).arg(sz.height()).toStdString());
 }
-inline NecroLog &operator<<(NecroLog log, const QRect &r) {
+inline NecroLog operator<<(NecroLog log, const QRect &r) {
 	QString s = "QRect(%1, %2, %3 x %4)";
 	return log.operator<<(s.arg(r.x()).arg(r.y())
 	                      .arg(r.width()).arg(r.height()).toStdString());
 }
-inline NecroLog &operator<<(NecroLog log, const QRectF &r) {
+inline NecroLog operator<<(NecroLog log, const QRectF &r) {
 	QString s = "QRect(%1, %2, %3 x %4)";
 	return log.operator<<(s.arg(r.x()).arg(r.y())
 	                      .arg(r.width()).arg(r.height()).toStdString());
