8c188a92cf74377c421ca50d9fd717d04d661d3e
arnost00 <arnost.p@centrum.cz>
2023-08-18 00:10:53 +0200

    CardReader - Show Bib (can use Bib in find), DISQ is readOnly, Can use Bib in "Assign card to runner"

--- quickevent/app/quickevent/plugins/CardReader/src/cardreaderwidget.cpp
+++ quickevent/app/quickevent/plugins/CardReader/src/cardreaderwidget.cpp
@@ -86,6 +86,7 @@ public:
 		col_classes_name,
 		col_competitorName,
 		col_competitors_registration,
+		col_competirors_bib,
 		col_runs_startTimeMs,
 		col_runs_timeMs,
 		col_runs_finishTimeMs,
@@ -114,12 +115,18 @@ Model::Model(QObject *parent)
 	setColumn(col_classes_name, ColumnDefinition("classes.name", tr("Class")));
 	setColumn(col_competitorName, ColumnDefinition("competitorName", tr("Name")));
 	setColumn(col_competitors_registration, ColumnDefinition("competitors.registration", tr("Reg")));
+	setColumn(col_competirors_bib, ColumnDefinition("competitors.startNumber", tr("Bib")));
 	setColumn(col_runs_startTimeMs, ColumnDefinition("runs.startTimeMs", tr("Start")).setCastType(qMetaTypeId<quickevent::core::og::TimeMs>()).setReadOnly(true));
 	setColumn(col_runs_timeMs, ColumnDefinition("runs.timeMs", tr("Time")).setCastType(qMetaTypeId<quickevent::core::og::TimeMs>()).setReadOnly(true));
 	setColumn(col_runs_finishTimeMs, ColumnDefinition("runs.finishTimeMs", tr("Finish")).setCastType(qMetaTypeId<quickevent::core::og::TimeMs>()).setReadOnly(true));
 	setColumn(col_runs_misPunch, ColumnDefinition("runs.misPunch", tr("Error")).setToolTip(tr("Card mispunch")).setReadOnly(true));
+<<<<<<< HEAD
 	setColumn(col_runs_disqualified, ColumnDefinition("runs.disqualified", tr("DISQ")).setToolTip(tr("Disqualified")));
 	setColumn(col_runs_cardLent, ColumnDefinition("cardLent", tr("RT")).setToolTip(tr("Card in rent table")).setReadOnly(true).setCastType(QMetaType::Bool));
+=======
+	setColumn(col_runs_disqualified, ColumnDefinition("runs.disqualified", tr("DISQ")).setToolTip(tr("Disqualified")).setReadOnly(true));
+	setColumn(col_runs_cardLent, ColumnDefinition("cardLent", tr("RT")).setToolTip(tr("Card in rent table")).setReadOnly(true).setCastType(QVariant::Bool));
+>>>>>>> 20f6fa9a (CardReader - Show Bib (can use Bib in find), DISQ is readOnly, Can use Bib in "Assign card to runner")
 	setColumn(col_runs_cardReturned, ColumnDefinition("runs.cardReturned", tr("R")).setToolTip(tr("Card returned")));
 	setColumn(col_cards_checkTime, ColumnDefinition("cards.checkTime", tr("CTIME")).setToolTip(tr("Card check time")).setReadOnly(true));
 	setColumn(col_cards_startTime, ColumnDefinition("cards.startTime", tr("STIME")).setToolTip(tr("Card start time")).setReadOnly(true));
@@ -408,7 +415,7 @@ void CardReaderWidget::reload()
 	qfs::QueryBuilder qb;
 	qb.select2("cards", "id, siId, runId, checkTime, startTime, finishTime, runIdAssignError")
 			.select2("runs", "id, startTimeMs, timeMs, finishTimeMs, misPunch, disqualified, cardReturned")
-			.select2("competitors", "registration")
+			.select2("competitors", "registration, startNumber")
 			.select2("classes", "name")
 			.select("COALESCE(lastName, '') || ' ' || COALESCE(firstName, '') AS competitorName")
 			.select("lentcards.siid IS NOT NULL OR runs.cardLent AS cardLent")
