f0e65a9164097f7f3e5dbb8290b67b57951e11df
Fanda Vacek <fvacek@elektroline.cz>
2023-08-27 21:54:20 +0200

    Cannot print report Competitors with rented cards #944 fixed

--- libqf/plugins/qf/core/src/qfcore_plugin.h
+++ libqf/plugins/qf/core/src/qfcore_plugin.h
@@ -4,15 +4,14 @@
 
 QT_BEGIN_NAMESPACE
 
-class QFQmlReportPlugin : public QQmlExtensionPlugin
+class QFCorePlugin : public QQmlExtensionPlugin
 {
 	Q_OBJECT
 	Q_PLUGIN_METADATA(IID QQmlExtensionInterface_iid)
 
 public:
-	QFQmlReportPlugin(QObject *parent = 0) : QQmlExtensionPlugin(parent) { }
+	QFCorePlugin(QObject *parent = 0) : QQmlExtensionPlugin(parent) { }
 	void registerTypes(const char *uri) override;
 };
 
 QT_END_NAMESPACE
-
